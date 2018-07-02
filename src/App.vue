f1768a0a5864a4e8442fce3ac97613d4c4404756
Michael Murphy <mmurphy@jobcase.com>
2018-07-01 21:50:22 -0400

    Setup basic template for game
    This is a rough draft. Things to add/change:
    - No btn actions
    - Scoreboard look and field
    - Logic
    - Use components

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,28 @@
 <template>
-  <div id="app">
+  <div class="container" id="app">
+    <div class=score-board>
+      <p>Away: {{ awayScore }}</p>
+      <p>Home: {{ homeScore }}</p>
+      <p>Inning: {{ inning }}</p>
+      <p>Outs: {{ outs }}</p>
+    </div>
+    <b-row>
+      <b-col md="4" offset-md="4">
+        <b-img src="/src/assets/field/000-field-view.png" fluid alt="Responsive image" />
+      </b-col>
+    </b-row>
+    <b-row class="text-center">
+      <template v-for="variant in actions">
+        <b-col md="4" offset-md="4">
+          <b-button variant="primary" class="play-action-btn">
+            {{variant}}
+          </b-button>
+        </b-col>
+      </template>
+      <b-col md="4" offset-md="4">
+        <b-button variant="danger" class="new-game-btn">New Game</b-button>
+      </b-col>
+    </b-row>
   </div>
 </template>
 
@@ -8,10 +31,24 @@ export default {
   name: 'app',
   data () {
     return {
+      actions: ['single','double','triple','home-run','steal','out'],
+      homeScore: 0,
+      awayScore: 0,
+      inning: 1,
+      outs: 0
     }
   }
 }
 </script>
 
 <style lang="scss">
+  .play-action-btn {
+    margin: 5px 0;
+    width: 100%;
+  }
+
+  .new-game-btn {
+    margin: 25px 0;
+    width: 100%;
+  }
 </style>
